631d57a32b4459cf73c72c51832067496069c223
fredpell1 <fredericpelletier7@gmail.com>
2022-01-29 11:30:44 -0500

    trash commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+import pygame as pg
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 #Hi everyone
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
