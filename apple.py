41b08fb20c4f52068020875edc35479deb4d1ab2
MatthewBeaulieu0 <matthew.beaulieu631@gmail.com>
2022-01-29 21:25:43 -0500

    Update appled.py

---
+++ apple.py
@@ -0,0 +1,21 @@
+
+import pygame as pg
+import random
+
+class apple(pg.sprite.Sprite):
+    def __init__(self, color = (255,0,0)):
+        pg.sprite.Sprite.__init__(self)
+        self.rect = (random.randrange(0, 400), random.randrange(0, 400), 25, 25)
+        self.image = pg.Surface([25, 25])
+        self.image.fill(color)
+        self.color = color
+        self.isEaten = False
+
+
+    def update(self):
+        if self.isEaten is True:
+            self.rect = (random.randrange(0, 400), random.randrange(0, 400), 25, 25)
+
+
+    def digest(self):
+        self.isEaten = True
